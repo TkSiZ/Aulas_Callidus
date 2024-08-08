d3299d561cc3ab7c1acd7c102ef85ab8bf776f04
Matheus Takashi Maruoka Vieira <149627412+TkSiZ@users.noreply.github.com>
2024-08-08 09:58:09 -0400

    Tudo certinho

---
+++ Node/aula4/acoes1.js
@@ -0,0 +1,17 @@
+let resp = window.document.getElementById('saida');
+
+function acao1(){
+    resp.innerHTML += '<p> Você clicou na ação 1 </p>'
+}
+
+function acao2(){
+    resp.innerHTML += '<p> Você clicou na ação 2 </p>'
+}
+
+function acao3(){
+    resp.innerHTML += '<p> Você clicou na ação 3 </p>'
+}
+
+function acao4(){
+    resp.innerHTML += '<p> Você clicou na ação 4 </p>'
+}
