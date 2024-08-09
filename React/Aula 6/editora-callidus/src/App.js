374f0b44be045e49d95c951813fb83746ee3a019
Matheus Takashi Maruoka Vieira <149627412+TkSiZ@users.noreply.github.com>
2024-08-09 11:46:42 -0400

    MAIS REACT

--- React/Aula 6/editora-callidus/src/App.js
+++ React/Aula 6/editora-callidus/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import TabelaHead from './components/TabelaHead';
+import TabelaBody from './components/TabelaBody';
+import TabelaFoot from './components/TabelaFoot';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    livros:[
+      {
+        id:"978-85-7522-632-2",
+        titulo:"CSS Grid Layout",
+        autor:"Maruício Samy Silva"
+      },
+      {
+        id:"978-85-7522-992-1",
+        titulo:"Node Essencial",
+        autor:"Matheus Takashi Maruoka Vieira"
+      },
+      {
+        id:"978-85-7522-521-7",
+        titulo:"Aprendendo Material Design",
+        autor:"Kyle Mew"
+      },
+    ]
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Tabela de Livros</h1>
+        <table>
+          <TabelaHead />
+          <TabelaBody livros={this.state.livros} />
+          <TabelaFoot qdelivros = {this.state.livros.length}/>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ React/Aula 6/editora-callidus/src/components/TabelaHead.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const TabelaHead = () => (
+    <thead>
+        <tr>
+            <td colSpan="4">Tabela de Livros</td>
+        </tr>
+        <tr>
+            <th>ISBN</th>
+            <th>Título</th>
+            <th>Autor</th>
+            <th></th>
+        </tr>
+    </thead>
+)
+
+export default TabelaHead;
