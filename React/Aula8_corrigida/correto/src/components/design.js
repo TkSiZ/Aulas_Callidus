d4f63947657c836173e5e6b2353a8ce488b0f8d5
Matheus Takashi Maruoka Vieira <149627412+TkSiZ@users.noreply.github.com>
2024-08-22 11:02:20 -0400

    node e react mais ou meno s

--- React/Aula8_corrigida/correto/src/components/design.js
+++ React/Aula8_corrigida/correto/src/components/design.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Design = () => {
+const Design = ({livros}) => {
     return (
-        <main className='principal'>
-            <h2>Categoria Design</h2>
-            <div className='card'>
-                <p>Conteúdos da página Design</p>
+    <main className='principal'>
+      <h2>Categoria Design</h2>
+      {livros
+        .filter((livro) => livro.categoria === 'design')
+        .map((livro) => (
+          <div className='card' key={livro.id}>
+            <div className='thumb'>
+              <img
+                src={`/imagens/capas/${livro.id}.jpg`} 
+                alt="Thumbnail da capa do livro..."
+              />
             </div>
-        </main>
+            <Link to={`/livro/${livro.slug}`}>
+              <div className='detalhes'>
+                <h3>{livro.titulo}</h3>
+                <p>{livro.descricao.slice(0, 130) + "..."}</p>
+                <p>Leia mais</p>
+              </div>
+            </Link>
+          </div>
+        ))}
+    </main>
     );
 }
 
